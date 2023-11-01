9d056bf1e8706613d547585b98af9b4c03d611b6
Jay <heyjay@cctplus.dev>
2023-11-01 17:02:56 -0500

    feat: add tailwind and mdx and update consts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = `Hey I'm Jay 👋`;
+export const SITE_DESCRIPTION = `You've found my spot on the internet. `;
