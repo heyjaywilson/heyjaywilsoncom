35ca0d8b0d76957cdd36b0cedd050e2d5f218bef
Jay <heyjay@cctplus.dev>
2023-12-06 09:25:21 -0600

    makes component and updates the rss feed

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -19,7 +19,7 @@ export const get = async () => {
     site: 'https://heyjaywilson.com',
     items: sortedPosts.map((post) => ({
       link: `/blog/${post.slug}`,
-      content: removeJSX(parser.render(post.body)),
+      content: removeJSX(parser.render(post.body), post.data.videoID ?? ""),
       title: post.data.metatitle ?? post.data.title,
       pubDate: post.data.updatedDate ?? post.data.pubDate,
       author: `heyjay@cctplus.dev`,
@@ -27,8 +27,12 @@ export const get = async () => {
   });
 };
 
-const removeJSX = (html: string) => {
+const makeYouTubeLink = (videoID: string) => {
+  return `<a href="https://www.youtube.com/watch?v=${videoID}">The video was embedded in the post. You can watch on YouTube.</a>`
+}
+const removeJSX = (html: string, videoID: string) => {
   return html
-    .replace(/<p>import\s(StaticTweet|Video)\sfrom\s'.*'<\/p>/gm, '')
-    .replace(/<p>&lt;(Video|StaticTweet).*\/&gt;<\/p>/gm, '');
+    .replace(/<p>import\s(YouTube|AffiliateDisclaimer)\sfrom\s'.*'<\/p>/gm, '')
+    .replace(/<p>&lt;(YouTube).*\/&gt;<\/p>/gm, makeYouTubeLink(videoID))
+    .replace(/<p>&lt;(AffiliateDisclaimer).*\/&gt;<\/p>/gm, '<p>Some links are affiliate links and I will receive some kind of commission if you purchase the service or product.</p>');
 };
