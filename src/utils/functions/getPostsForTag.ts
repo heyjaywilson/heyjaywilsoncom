7867f3826bcfa7603c813cc1fd70c898d7d16fad
Jay <heyjay@cctplus.dev>
2023-12-14 16:41:19 -0600

    feat: tags in post

---
+++ src/utils/functions/getPostsForTag.ts
@@ -0,0 +1,11 @@
+import { type CollectionEntry } from "astro:content";
+import { slugifyAll } from "./slugify";
+
+const getPostsForTag = (posts: CollectionEntry<"blog">[], tag: string) => {
+  const filteredPosts = posts.filter((post) =>
+    slugifyAll(post.data.tags).includes(tag)
+  );
+  return filteredPosts
+};
+
+export default getPostsForTag;
