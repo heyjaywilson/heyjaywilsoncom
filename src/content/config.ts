9827627f5a4451bda320669f0288152221a7ff1d
Jay Wilson <heyjay@cctplus.dev>
2024-02-21 22:32:15 -0600

    feat: webmention at bottom of post (#32)
    - **wip**
    - **feat: webmentions**
    - **chore: update package**

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,33 +1,39 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
+import { webMention } from "./../utils/types/Webmention";
 
 const blog = defineCollection({
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		metatitle: z.string().optional(),
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-		canonicalURL: z.string().optional(),
-		videoID: z.string().optional(),
-		isFeatured: z.boolean().optional(),
-		includeBMC: z.boolean().default(true),
-		tags: z.array(z.string()).default(["other"]),
-	}),
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    metatitle: z.string().optional(),
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+    canonicalURL: z.string().optional(),
+    videoID: z.string().optional(),
+    isFeatured: z.boolean().optional(),
+    includeBMC: z.boolean().default(true),
+    tags: z.array(z.string()).default(["other"]),
+  }),
 });
 
 const workExperience = defineCollection({
-	schema: z.object({
-		position: z.string(),
-		employer: z.string(),
-		website: z.string().optional(),
-		startDate: z.coerce.date(),
-		endDate: z.coerce.date().optional(),
-		icon: z.string(),
-		isPresent: z.boolean(),
-	})
-})
+  schema: z.object({
+    position: z.string(),
+    employer: z.string(),
+    website: z.string().optional(),
+    startDate: z.coerce.date(),
+    endDate: z.coerce.date().optional(),
+    icon: z.string(),
+    isPresent: z.boolean(),
+  }),
+});
 
-export const collections = { blog, workExperience };
+const webmentions = defineCollection({
+  type: "data",
+  schema: z.array(webMention),
+});
+
+export const collections = { blog, workExperience, webmentions };
