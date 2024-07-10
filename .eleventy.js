29eb03ab307fd9016f5cd5a1d090de2851cc61b2
Jay <heyjay@cctplus.dev>
2024-07-09 21:52:56 -0500

    base styles and page

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,14 @@
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/assets/css/*");
+  eleventyConfig.addGlobalData("currentYear", () => (new Date()).getFullYear());
+
   return {
     dir: {
       input: "src",
       output: "public",
+      data: "_data",
       includes: "_includes",
+      layouts: "_layouts",
     },
   };
 };
