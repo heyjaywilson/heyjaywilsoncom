d11a7e1d4f4391ec9fdef57c4fedc81805bd216b
Jay Wilson <heyjay@cctplus.dev>
2024-08-14 13:35:26 -0500

    post: make post for find relief (#46)

--- src/utils/functions/getUniqueTags.ts
+++ src/utils/functions/getUniqueTags.ts
@@ -2,8 +2,7 @@ import { slugifyStr } from "./slugify";
 import { type CollectionEntry } from "astro:content";
 
 const getUniqueTags = (posts: CollectionEntry<"blog">[]) => {
-  const filteredPosts = posts.filter(({ data }) => !data.draft);
-  const tags: string[] = filteredPosts
+  const tags: string[] = posts
     .flatMap((post) => post.data.tags)
     .map((tag) => slugifyStr(tag))
     .filter(
